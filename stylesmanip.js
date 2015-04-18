5831434d93e6cb99720de108a56d2239355ddda1
Galen Chuang <gchuang@wellesley.edu>
2015-04-18 13:47:53 -0400

    stylesmanip stuff

--- stylesmanip.js
+++ stylesmanip.js
@@ -1,3 +1,22 @@
 // 1. Iterate through each element on the page
 // 2. Replace specified color with another specified color (with margin)
 
+//alert("reaching stylesmanip")
+
+$( document.body ).click(function() {
+  $( "[id^='div']" ).each(function(i) {
+  	console.log("style: " + $(this).css('background-color'));
+    if ($(this).css('background-color') == 'rgb(0, 0, 255)') {
+      $(this).css({background: 'yellow'});
+    //} else {
+      //$(this).css({background: 'blue'});
+    }
+  });
+});
+
+//for testing
+function display(msg) {
+    var p = document.createElement('p');
+    p.innerHTML = msg;
+    document.body.appendChild(p);
+}
