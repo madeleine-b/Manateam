c5940dc62c4f109c5dada048828deae7d9b0bcd4
madeleine-b <madeleine-b@users.noreply.github.com>
2015-04-19 00:59:12 -0400

    rescaling working

--- colormanip.js
+++ colormanip.js
@@ -1,27 +1,17 @@
-/*
-	TODO: Also adjust contrast, vibracy, etc. to setting
 
-	.saturate {-webkit-filter: saturate(3);}
-	.grayscale {-webkit-filter: grayscale(100%);}
-	.contrast {-webkit-filter: contrast(160%);}
-	.brightness {-webkit-filter: brightness(0.25);}
-	.blur {-webkit-filter: blur(3px);}
-	.invert {-webkit-filter: invert(100%);}
-	.sepia {-webkit-filter: sepia(100%);}
-	.huerotate {-webkit-filter: hue-rotate(180deg);}
-	.rss.opacity {-webkit-filter: opacity(50%);}
-*/
 function createCanvas(img) {
     var canvas = document.createElement('canvas');
 
-    canvas.width = img.width;
-    canvas.height = img.height;
+    //canvas.width = img.width;
+    //canvas.height = img.height;
+
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+
     canvas.style.width = canvas.width + "px";
     canvas.style.height = canvas.height + "px";
-    canvas.style.top = getPosition.y;
+    canvas.style.top = getPosition(img).y;
     canvas.style.left = getPosition(img).x;
-
-    img.parentNode.insertBefore(canvas, img.nextSibling);
 
     return canvas;
 }
@@ -39,8 +29,8 @@ function getPosition(element) {
 
 function fixPicsInDoc() {
 	var images = $('img');
-	console.log("fixPics executes on "+images.length+" images");
-	
+	//console.log("fixPics executes on "+images.length+" images");
+
 	var colorToReplace = (localStorage.getItem("gColorIn")!='undefined' ? localStorage.getItem("gColorIn") : "#BE2A3A");
 	var replacementColor = (localStorage.getItem("gColorOut")!="undefined" ? localStorage.getItem("gColorOut") : "#6f47e1");
 	G_COLOR_IN_MARGIN = (localStorage.getItem("tolIn")!="undefined" ? localStorage.getItem("tolIn") : "150"); //100 to 200
@@ -75,8 +65,7 @@ function colorReplace(img) {
 	//var imageX = getAbsPosition(img)[1], imageY = getAbsPosition(img)[0];
 	
 	context.drawImage(img, 0, 0);
-
-	var image = context.getImageData(0, 0, img.width, img.height);
+	var image = context.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
  
 	for (var i = 0, n = image.data.length; i < n; i += 4) {
 		//3D RGB space mapping :)
@@ -88,8 +77,13 @@ function colorReplace(img) {
 		//i+3 is alpha channel for opacity
 	}
 
-	img.parentNode.removeChild(img);
 	context.putImageData(image, 0, 0);
+	canvas.style.width = img.width+"px";
+	canvas.style.height = img.height+"px";
+
+	img.parentNode.insertBefore(canvas, img.nextSibling);
+	img.parentNode.removeChild(img);
+
 	console.log("image replacement success");
 }
 
@@ -105,30 +99,4 @@ function colorWithinRange(varMargin, imageR, imageG, imageB, toReplace) {
 	return distanceBetween<=varMargin;
 }
 
-
-//thanks @SO
-/*
-function getAbsPosition (el){
-    var el2 = el;
-    var curtop = 0;
-    var curleft = 0;
-    if (document.getElementById || document.all) {
-        do  {
-            curleft += el.offsetLeft-el.scrollLeft;
-            curtop += el.offsetTop-el.scrollTop;
-            el = el.offsetParent;
-            el2 = el2.parentNode;
-            while (el2 != el) {
-                curleft -= el2.scrollLeft;
-                curtop -= el2.scrollTop;
-                el2 = el2.parentNode;
-            }
-        } while (el.offsetParent);
-
-    } else if (document.layers) {
-        curtop += el.y;
-        curleft += el.x;
-    }
-    return [curtop, curleft];
-}*/
 
