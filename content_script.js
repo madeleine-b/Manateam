c44173abdfddbda40efac0d65de72857d786b507
jchristy-lo <achristy@wellesley.edu>
2015-04-19 01:34:18 -0400

    trim comments
    deleted some old try/fail in the content_script

--- content_script.js
+++ content_script.js
@@ -6,20 +6,3 @@ chrome.runtime.sendMessage(DOMImages, function (response) {
 	console.log("sent off images and got response");
 	console.log(response);
 });
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-// alert("line 5");
-// chrome.runtime.reload();
-// alert("line 7");
-// chrome.tabs.query({ url: "<all_urls>"}, function(tabs)
-// {
-//     for(var i = 0; i < tabs.length; i++)
-//     {
-//         // chrome.tabs.executeScript(tabs[i].id, 
-//         	// { code: alert(reaches background); })
-//         		// { file: "colormanip.js" }, colorReplaceStuff());
-//     	chrome.tabs.executeScript(tabs[i].id, 
-//     			{ file: "stylesmanip.js" }, replaceStyle() {});
-//     }
-// }
-// });
