504afd30026df9923855c4025a5310a48cae14c6
Galen Chuang <gchuang@wellesley.edu>
2015-04-18 23:33:51 -0400

    updating stylesmanip.js

--- stylesmanip.js
+++ stylesmanip.js
@@ -2,32 +2,54 @@
 //(div, text, background, etc) with a defined color.
 
 // TODO:
+// -What to do with text (and background?) colors that haven't been set? They appear as rgb(0,0,0)...
 // -Do for elements other than divs
 // -Note: don't think we need margin for replaceWith, because each element will have uniform color
 
+
 var MARGIN = 150;
-var toReplace = {"r" : 0, "g" : 0, "b" : 255};
-var replaceWith = "rgb(255, 255, 0)";
+var toReplace = {"r" : 0, "g" : 0, "b" : 200};
+var replaceWith = "rgb(255, 0, 0)";
 var backgroundRGB = [''];
 var colorRGB = [''];
   
 //goes through every div/p/h1, etc in the page and replaces specified color with yellow (hard coded for now)
 $(document).ready(function() {
   var all = document.getElementsByTagName("*");
+
+
     for (var i=0, max=all.length; i < max; i++) {
       backgroundRGB = splitRGB($(all[i]).css('background-color'));
-      if (colorWithinRange(MARGIN, backgroundRGB[1], backgroundRGB[2], backgroundRGB[3], toReplace)) {
+      console.log("background: " + $(all[i]).css('background-color'));
+      if (colorWithinRange(MARGIN, backgroundRGB[1], backgroundRGB[2], backgroundRGB[3], toReplace)){
         $(all[i]).css({'background-color': replaceWith});
       }
     }
 
     for (var i=0, max=all.length; i < max; i++) {
       colorRGB = splitRGB($(all[i]).css('color'));
-      if (colorWithinRange(MARGIN, colorRGB[1], colorRGB[2], colorRGB[3], toReplace)) {
+      console.log("color: " + $(all[i]).css('color'));
+      if (colorWithinRange(MARGIN, colorRGB[1], colorRGB[2], colorRGB[3], toReplace)
+        || colorNotSet(colorRGB)) { //second case trying to take care of color not set
         $(all[i]).css({'color': replaceWith});
       }
     }
 });
+
+//if color was not set... apparently this is rgb(0,0,0) which equals white??
+function colorNotSet(RGBarray) {
+  //console.log((RGBarray[1] == 0) && (RGBarray[2] == 0) && (RGBarray[3] == 0));
+  return ((RGBarray[1] == 0) && (RGBarray[2] == 0) && (RGBarray[3] == 0));
+}
+
+function hexToRGB(h) {
+    var rgb = [hexToR(h), hexToG(h), hexToB(h)}];
+    return rgb;
+  }
+  function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+  function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+  function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+  function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 
 //stolen from madeleine - checks if web colors are within range of chosen color to replace
 function colorWithinRange(margin, webR, webG, webB, toReplace) {
@@ -35,29 +57,9 @@ function colorWithinRange(margin, webR, webG, webB, toReplace) {
 	return distanceBetween<=margin;
 }
 
-// //converts hex back to rgb
-// function hexToRGB(h) {
-// 		var rgb = {"r" : hexToR(h), "g" : hexToG(h), "b" : hexToB(h)};
-// 		return rgb;
-// 	}
-// 	function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
-// 	function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
-// 	function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
-// 	function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
-
 function splitRGB(rgb) {
   var split = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
   var splitArray = split.exec(rgb);
   return splitArray;
   //console.log("in splitRGB, splitArray: " + splitArray);
 }
-
-// //converts rgb to hex (because i am dum but it's ok it works)
-// function rgb2hex(rgb) {
-//     if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
-//     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-//     function hex(x) {
-//         return ("0" + parseInt(x).toString(16)).slice(-2);
-//     }
-//     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-// }
