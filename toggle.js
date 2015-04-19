2d73bae681b3cdb4073debbb8275516a147fc02c
madeleine-b <madeleine-b@users.noreply.github.com>
2015-04-19 02:14:49 -0400

    fix indentation toggle

--- toggle.js
+++ toggle.js
@@ -1,73 +1,86 @@
 function init() {
-var cb = document.getElementById("cb");
-if(cb){
-cb.addEventListener("click", function() {handleClick(cb);});} //checkbox
-colorIn = document.getElementById("ColorIn");
-colorInRange = document.getElementById("ColorInRange");
-colorOut = document.getElementById("ColorOut");
-colorOutRange = document.getElementById("ColorOutRange");
-if(colorIn){
-colorIn.addEventListener("input",function() {outputUpdate(colorIn.value,1);});}
-if(colorInRange){
-colorInRange.addEventListener("input",function() {outputUpdate(colorInRange.value,2);});}
-if(colorOut){
-colorOut.addEventListener('input',function() {outputUpdate(colorOut.value,3);});}
-if(colorOutRange){
-colorOutRange.addEventListener('input',function() {outputUpdate(colorOutRange.value,4);});}
-if (localStorage.getItem("currentState")!="undefined") {
-if(cb){
-cb.checked = (localStorage.getItem("currentState")=="on" ? true : false);}
-} else {
-cb.checked = false;
+  var cb = document.getElementById("cb");
+  if(cb){
+    cb.addEventListener("click", function() {handleClick(cb);});
+  } //checkbox
+  colorIn = document.getElementById("ColorIn");
+  colorInRange = document.getElementById("ColorInRange");
+  colorOut = document.getElementById("ColorOut");
+  colorOutRange = document.getElementById("ColorOutRange");
+  if(colorIn){
+    colorIn.addEventListener("input",function() {outputUpdate(colorIn.value,1);});
+  }
+  if(colorInRange){
+    colorInRange.addEventListener("input",function() {outputUpdate(colorInRange.value,2);});
+  }
+  if(colorOut){
+    colorOut.addEventListener('input',function() {outputUpdate(colorOut.value,3);});}
+
+  if(colorOutRange){
+    colorOutRange.addEventListener('input',function() {outputUpdate(colorOutRange.value,4);});
+  }
+  if (localStorage.getItem("currentState")!="undefined") {
+    if(cb){
+      cb.checked = (localStorage.getItem("currentState")=="on" ? true : false);
+    }
+  } else {
+      cb.checked = false;
+  }
+  if(colorIn){
+      colorIn.value = (typeof localStorage.getItem("gColorIn")!="undefined" ? localStorage.getItem("gColorIn") : "#ff0000");
+  }
+  if(colorInRange){
+    colorInRange.value = (typeof localStorage.getItem("tolIn")!="undefined" ? localStorage.getItem("tolIn") : "100");
+  }
+  if(colorOut){
+    colorOut.value = (typeof localStorage.getItem("gColorOut")!="undefined" ? localStorage.getItem("gColorOut") : "#ff0000");
+  }
+  if(colorOutRange){
+    colorOutRange.value = (typeof localStorage.getItem("tolOut")!="undefined" ? localStorage.getItem("tolOut") : "100");
+  }
 }
-if(colorIn){
-colorIn.value = (typeof localStorage.getItem("gColorIn")!="undefined" ? localStorage.getItem("gColorIn") : "#ff0000");}
-if(colorInRange){
-colorInRange.value = (typeof localStorage.getItem("tolIn")!="undefined" ? localStorage.getItem("tolIn") : "100");}
-if(colorOut){
-colorOut.value = (typeof localStorage.getItem("gColorOut")!="undefined" ? localStorage.getItem("gColorOut") : "#ff0000");}
-if(colorOutRange){
-colorOutRange.value = (typeof localStorage.getItem("tolOut")!="undefined" ? localStorage.getItem("tolOut") : "100");}
-}
+
 window.addEventListener('DOMContentLoaded', function() {init();});
+
 //Toggles colormanip on the page when button clicked
 function handleClick(cb) {
-localStorage.setItem("currentState", (cb.checked==true ? "on" : "off"));
-console.log("currentState is now " + localStorage.getItem("currentState"));
-if (cb.checked) {
-console.log("checked");
-values = {'doFxn':true, 'colorIn': colorIn,'colorInRange':colorInRange, 'colorOut':colorOut, 'colorOutRange':colorOutRange};
-chrome.runtime.sendMessage(
-{'method':'getValues', 'values':values},
-function(response){
-console.log("response=");
-console.log(response);
+  localStorage.setItem("currentState", (cb.checked==true ? "on" : "off"));
+  console.log("currentState is now " + localStorage.getItem("currentState"));
+  if (cb.checked) {
+    console.log("checked");
+    values = {'doFxn':true, 'colorIn': colorIn,'colorInRange':colorInRange, 'colorOut':colorOut, 'colorOutRange':colorOutRange};
+    chrome.runtime.sendMessage(
+      {'method':'getValues', 'values':values},
+      function(response){
+        console.log("response=");
+        console.log(response);
+      }
+    );
+  //fixPicsInDoc(images);
+  } else {
+    console.log("unchecked");
+    values = {'doFxn':false, 'colorIn': colorIn,'colorInRange':colorInRange, 'colorOut':colorOut, 'colorOutRange':colorOutRange};
+  }
 }
-);
-//fixPicsInDoc(images);
-} else {
-console.log("unchecked");
-values = {'doFxn':false, 'colorIn': colorIn,'colorInRange':colorInRange, 'colorOut':colorOut, 'colorOutRange':colorOutRange};
-}
-}
+
 function outputUpdate(val, num) {
-console.log(val);
-switch(num) {
-case 1:
-localStorage.setItem("gColorIn",val);
-break;
-case 2:
-localStorage.setItem("tolIn",val);
-break;
-case 3:
-localStorage.setItem("gColorOut",val);
-break;
-case 4:
-localStorage.setItem("tolOut", val);
-break;
-default:
-console.log("Huh, weirdness with popup and local storage");
-}
+  console.log(val);
+  switch(num) {
+    case 1:
+      localStorage.setItem("gColorIn",val);
+      break;
+    case 2:
+      localStorage.setItem("tolIn",val);
+      break;
+    case 3:
+      localStorage.setItem("gColorOut",val);
+      break;
+    case 4:
+      localStorage.setItem("tolOut", val);
+      break;
+    default:
+      console.log("Huh, weirdness with popup and local storage");
+  }
 }
 /*
 TODO: Also adjust contrast, vibracy, etc. to setting
